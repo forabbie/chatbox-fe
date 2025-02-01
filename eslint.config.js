3be5b4383462bda603db433d23e5dd387e3d74ce
abbie <supermercadoabbie@gmail.com>
2025-02-01 19:27:53 +0800

    fix: migrate ESLint config to ES modules and resolve parser error

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,25 @@
-import vue from "eslint-plugin-vue";
-import prettier from "eslint-config-prettier";
+import vuePlugin from 'eslint-plugin-vue'
+import prettier from 'eslint-config-prettier'
+import vuePrettierConfig from '@vue/eslint-config-prettier'
+import vueEslintParser from 'vue-eslint-parser' // Import the parser directly
 
 export default [
   {
-    files: ["**/*.vue", "**/*.js", "**/*.ts"],
+    files: ['**/*.vue', '**/*.js', '**/*.ts'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module"
+      parser: vueEslintParser, // Use the imported parser
+      ecmaVersion: 'latest',
+      sourceType: 'module'
     },
     plugins: {
-      vue
+      vue: vuePlugin
     },
     rules: {
-      "vue/multi-word-component-names": "off",
-      "vue/script-setup-uses-vars": "error",
-      "no-unused-vars": ["error", { vars: "all", args: "after-used", ignoreRestSiblings: false }]
+      'vue/multi-word-component-names': 'off',
+      'vue/script-setup-uses-vars': 'error',
+      'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }]
     }
   },
-  prettier
-];
+  prettier,
+  vuePrettierConfig
+]
