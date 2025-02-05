9c4add597e86de8f0014065f43f8674339f896dd
abbie <supermercadoabbie@gmail.com>
2025-02-05 22:34:18 +0800

    feat: add base in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
