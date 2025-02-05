31f0407dabc65c63704654105416ba44196273fd
abbie <supermercadoabbie@gmail.com>
2025-02-05 23:17:08 +0800

    chore: update alias and base in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import path from 'path'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [vue()],
-    base: mode === 'production' ? '/chatbox-fe' : '/',
+    base: mode === 'production' ? '/' : '/',
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': path.resolve(__dirname, 'src')
       }
     }
   }
