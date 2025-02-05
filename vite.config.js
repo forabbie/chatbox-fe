5bf4938663586e9d99c561c697a96291e395ea75
abbie <supermercadoabbie@gmail.com>
2025-02-06 00:00:08 +0800

    fix: build paths

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src')
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/chatbox-fe' : '/' // Replace 'repo-name' with your actual repository name
+  base: process.env.NODE_ENV === 'production' ? '/chatbox-fe' : '/'
 })
