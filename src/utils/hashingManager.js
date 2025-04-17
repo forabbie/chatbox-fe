2013ec8d833367369ff35c5ef1a575ec252be727
abbie <supermercadoabbie@gmail.com>
2025-04-17 20:15:23 +0800

    Feat: setup storage and hashing utils

---
+++ src/utils/hashingManager.js
@@ -0,0 +1,90 @@
+import CryptoJS from 'crypto-js'
+
+// Define the hexDecode function
+const hexDecode = (s) => CryptoJS.enc.Hex.parse(s)
+
+// Define the hexEncode function
+const hexEncode = (b) => CryptoJS.enc.Hex.stringify(b)
+
+// Encrypts the message using AES encryption
+export const encryptMessage = (value) => {
+  try {
+    const decodedKey = hexDecode(import.meta.env.VITE_APP_ID_KEY)
+    const iv = CryptoJS.lib.WordArray.random(16)
+    const ciphertext = CryptoJS.AES.encrypt(value.toString(), decodedKey, {
+      iv: iv,
+      mode: CryptoJS.mode.CFB,
+      padding: CryptoJS.pad.NoPadding
+    })
+
+    const concatenated = iv.clone().concat(ciphertext.ciphertext)
+    return hexEncode(concatenated)
+  } catch (error) {
+    console.error('Encryption error:', error)
+    throw error
+  }
+}
+
+// Define the decryptMessage function
+export const decryptMessage = (encryptedHex) => {
+  try {
+    const decodedKey = hexDecode(import.meta.env.VITE_APP_ID_KEY)
+    const encryptedWA = hexDecode(encryptedHex)
+    const iv = CryptoJS.lib.WordArray.create(encryptedWA.words.slice(0, 4))
+    const ciphertext = CryptoJS.lib.WordArray.create(encryptedWA.words.slice(4))
+    const decrypted = CryptoJS.AES.decrypt({ ciphertext: ciphertext }, decodedKey, {
+      iv: iv,
+      mode: CryptoJS.mode.CFB,
+      padding: CryptoJS.pad.NoPadding
+    })
+
+    return decrypted.toString(CryptoJS.enc.Utf8)
+  } catch (error) {
+    console.error('Decryption error:', error)
+    throw error
+  }
+}
+
+// Function to serialize data before encryption
+const serializeData = (data) => JSON.stringify(data)
+
+// Function to deserialize data after decryption
+const deserializeData = (encryptedData) => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(encryptedData, import.meta.env.VITE_APP_ID_KEY)
+    const decryptedData = bytes.toString(CryptoJS.enc.Utf8)
+    return JSON.parse(decryptedData)
+  } catch (error) {
+    console.error('Error deserializing data:', error)
+    return null
+  }
+}
+
+export const encryptCookie = (message) => {
+  try {
+    const serializedMessage = serializeData(message)
+    return CryptoJS.AES.encrypt(serializedMessage, import.meta.env.VITE_APP_ID_KEY).toString()
+  } catch (error) {
+    console.error('Error encrypting message:', error)
+    return null
+  }
+}
+
+export const decryptCookie = (encryptedMessage) => {
+  try {
+    return deserializeData(encryptedMessage)
+  } catch (error) {
+    console.error('Error decrypting message:', error)
+    return null
+  }
+}
+
+// Base64 encryption
+export const utf8ToB64 = (str = '') => {
+  return btoa(unescape(encodeURIComponent(str)))
+}
+
+// decryption of Base64
+export const b64ToUtf8 = (str = '') => {
+  return decodeURIComponent(escape(atob(str)))
+}
--- src/utils/storageManager.js
+++ src/utils/storageManager.js
@@ -1,3 +1,7 @@
+import { encryptCookie, decryptCookie } from './hashingManager'
+
+const isProduction = import.meta.env.VITE_APP_ENV === 'production'
+
 export const setLocalStorage = (key, str) => {
   const value = JSON.stringify(str)
   const encode = utf8ToB64(value)
@@ -36,4 +40,63 @@ export const getSessionStorage = (key = '', isEncrypt = true) => {
 
 export const deleteSessionStorage = (key = '') => {
   sessionStorage.removeItem(key)
+}
+
+// Function to set a cookie with encryption
+export const setCookie = (name, value, minutes, sameSite = 'Strict') => {
+  try {
+    const encryptedValue = encryptCookie(value)
+    if (!encryptedValue) {
+      throw new Error('Encryption failed')
+    }
+
+    let cookie = `${name}=${encodeURIComponent(encryptedValue)}; path=/`
+
+    if (minutes) {
+      const expires = new Date(Date.now() + minutes * 60 * 1000).toUTCString()
+      cookie += `; expires=${expires}`
+    }
+
+    if (isProduction) {
+      cookie += '; Secure'
+    }
+
+    if (sameSite) {
+      cookie += `; SameSite=${sameSite}`
+    }
+
+    document.cookie = cookie.trim()
+  } catch (error) {
+    console.error('Error encrypting and setting cookie:', error)
+    throw error
+  }
+}
+
+// Function to get a cookie by name and decrypt it
+export const getCookie = (name) => {
+  const nameEQ = `${name}=`
+  const cookies = document.cookie.split(';').map((cookie) => cookie.trim())
+
+  for (const cookie of cookies) {
+    if (cookie.startsWith(nameEQ)) {
+      const encryptedValue = cookie.substring(nameEQ.length)
+
+      try {
+        const decryptedValue = decryptCookie(decodeURIComponent(encryptedValue))
+        if (!decryptedValue) {
+          throw new Error('Decryption failed')
+        }
+        return decryptedValue
+      } catch (error) {
+        console.error('Error decrypting cookie:', error)
+        return null
+      }
+    }
+  }
+  return null
+}
+
+// Function to erase a cookie by name
+export const eraseCookie = (name) => {
+  document.cookie = `${name}=; Max-Age=0; path=/; Secure; SameSite=Strict`
 }
